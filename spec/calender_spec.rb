667205158a7965c22e81eeefb6c479403053fcc1
abdullah-siddiqui-git <muhammad.abdullah@7vals.com>
2019-07-02 19:07:14 +0500

    Added more tescases.

--- spec/calender_spec.rb
+++ spec/calender_spec.rb
@@ -30,4 +30,21 @@ describe Calender do
     # At this point of code, event must be changed.
     expect( calender.get_list_of_events( date )[ -1 ].name ).to eq( "Event 1 - changed" )
   end
+
+  it "Check if deletion works properly" do
+    # Add a new event and check if changes
+    event = Event.new( "Event 1", "Event 1 description." )
+    date  = Date.new( 2019, 7, 2 )
+
+    calender.register_event! date, event
+
+    # Delete this event from the calender and try to search it again.
+    # Added event will be present at the end of the array.
+    # -1 as index refers to the end of the array. 
+    calender.delete_event!( date, -1 )
+
+    # Check if the event object is still present in the list of events.
+    result = calender.get_list_of_events( date ).select { |event_loop| event_loop.object_id == event.object_id }
+    expect( result.length ).to eq( 0 )
+  end
 end
